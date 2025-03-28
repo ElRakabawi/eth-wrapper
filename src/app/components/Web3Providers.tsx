2180a23f8696019726d453d246635b836bd153e6
Muhammed <mselrakabawi@gmail.com>
2025-03-28 10:51:56 +0400

    feat: safe SCW support

--- src/app/components/Web3Providers.tsx
+++ src/app/components/Web3Providers.tsx
@@ -4,6 +4,7 @@ import { darkTheme, getDefaultWallets, RainbowKitProvider } from '@rainbow-me/ra
 import { WagmiProvider, createConfig, http } from 'wagmi';
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { sepolia } from 'wagmi/chains';
+import { safe } from 'wagmi/connectors';
 
 const queryClient = new QueryClient();
 const sepoliaTransport = http('https://gateway.tenderly.co/public/sepolia');
@@ -13,13 +14,18 @@ const { connectors } = getDefaultWallets({
   projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || '',
 });
 
-
 export const wagmiConfig = createConfig({
   chains: [sepolia],
   transports: {
     [sepolia.id]: sepoliaTransport,
   },
-  connectors,
+  connectors: [
+    safe({
+      allowedDomains: [/app.safe.global$/],
+      debug: process.env.NODE_ENV === 'development',
+    }),
+    ...connectors,
+  ],
   ssr: true,
 });
 
@@ -27,7 +33,11 @@ export default function Web3Providers({ children }: { children: React.ReactNode 
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider theme={darkTheme()} modalSize="wide">
+        <RainbowKitProvider 
+          theme={darkTheme()} 
+          modalSize="wide"
+          initialChain={sepolia}
+        >
           {children}
         </RainbowKitProvider>
       </QueryClientProvider>
